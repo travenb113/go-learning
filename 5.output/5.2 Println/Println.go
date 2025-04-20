6ca137ab3d02a9350c47aeb98f687934bb046a78
travenb113 <Overwhelmingevil@proton.me>
2025-04-20 07:15:47 +0300

    Types of print

---
+++ 5.output/5.2 Println/Println.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b string = "Hello", "World"
+	fmt.Println(a, b)
+}
---
+++ 5.output/5.3 Printf/Printf.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "Hello"
+	var b int = 15
+
+	fmt.Printf("a has value: %v and type: %T\n", a, a)
+	fmt.Printf("b has value: %v and type: %T", b, b)
+}

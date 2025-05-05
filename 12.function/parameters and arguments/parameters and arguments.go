e4037ea9747378a192834740a06bf5371fa820b9
travenb113 <Overwhelmingevil@proton.me>
2025-05-05 11:28:51 +0300

    Add example demonstrating function parameters and arguments

---
+++ 12.function/parameters and arguments/parameters and arguments.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func familyName(fname string) {
+	fmt.Println("Hello", fname, "Refsnes")
+}
+
+func main() {
+	familyName("Liam")
+	familyName("Jenny")
+	familyName("Anja")
+}

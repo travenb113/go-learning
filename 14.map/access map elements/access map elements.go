5023588c911b5a9c87e2b21502697bcce4129157
travenb113 <Overwhelmingevil@proton.me>
2025-05-11 23:35:47 +0300

    Simple example of access map elements

---
+++ 14.map/access map elements/access map elements.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = make(map[string]string)
+	a["brand"] = "Ford"
+	a["model"] = "Mustang"
+	a["year"] = "1964"
+
+	fmt.Printf(a["brand"])
+}
